6b3dee42b4c91ba603bd8acad2ce118b4f611d11
ekyss <ikermorera2004@gmail.com>
2026-02-20 19:51:09 +0100

    commit inicial

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("dev.langchain4j:langchain4j-bom:1.10.0"))
+    implementation("dev.langchain4j:langchain4j-open-ai")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
