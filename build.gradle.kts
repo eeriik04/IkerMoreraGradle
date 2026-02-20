6394fb8550bfdd8d12778216627ef9f54cabba7d
ekyss <ikermorera2004@gmail.com>
2026-02-20 20:12:28 +0100

    commit historial de mensajes

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-simple:2.0.12")
     implementation(platform("dev.langchain4j:langchain4j-bom:1.10.0"))
     implementation("dev.langchain4j:langchain4j-open-ai")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,29 @@
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 
- class main {
+import java.util.ArrayList;
+import java.util.List;
+
+class main {
     public static void main(String[] args) {
-
+        // El TOKEN no es necesario para interactuar con modelos locales
+        final String TOKEN = "PEGA_AQUI_TU_TOKEN";
+        var model = OpenAiChatModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey(TOKEN)
+                .modelName("gemma:2b")
+                .build();
+        List<ChatMessage> history = new ArrayList<>();
+        // Interacción 1
+        history.add(new UserMessage("Hola, soy Germán"));
+        AiMessage respuesta = model.chat(history).aiMessage();
+        history.add(respuesta);
+        // Interacción 2
+        history.add(new UserMessage("¿Recuerdas cómo me llamo?"));
+        respuesta = model.chat(history).aiMessage();
+        history.add(respuesta);
+        System.out.println(respuesta.text());
     }
-}
+ }
